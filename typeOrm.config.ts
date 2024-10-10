9eef5118a6fb92697ea37dc2a06fc875d368dc8e
thesunnatillo <sunnatillocoder@gmail.com>
2024-10-10 21:53:11 +0500

    new file

---
+++ typeOrm.config.ts
@@ -0,0 +1,13 @@
+import { DataSource } from "typeorm";
+import { UserEntity } from './src/user/entitys/user.entity';
+
+export default new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '7582',
+  database: 'users_db',
+  migrations: ['migrations/**'],
+  entities: [UserEntity],
+})
